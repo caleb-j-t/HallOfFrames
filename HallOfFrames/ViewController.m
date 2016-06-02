a2ab90f14fa5515cb6556a9ee4159f969475b2f9
Caleb Talbot <mailcaleb@gmail.com>
2016-06-02 15:41:21 -0500

    start step 2

--- HallOfFrames/ViewController.m
+++ HallOfFrames/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 #import "Picture.h"
 #import <UIKit/UIKit.h>
+#import "PictureCollectionViewCell.h"
 
 @interface ViewController () <UICollectionViewDelegate, UICollectionViewDataSource>
 @property NSMutableArray *galleryPhotos;
@@ -18,14 +19,23 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSMutableArray *galleryPhotos = [[NSMutableArray alloc]initWithObjects:[UIImage imageNamed:@"Picture1"],[UIImage imageNamed:@"Picture2"],[UIImage imageNamed:@"Picture3"],[UIImage imageNamed:@"Picture4"],[UIImage imageNamed:@"Picture5"], nil];
-    
+//    self.galleryPhotos = [[NSMutableArray alloc]initWithObjects:[UIImage imageNamed:@"Picture1"],[UIImage imageNamed:@"Picture2"],[UIImage imageNamed:@"Picture3"],[UIImage imageNamed:@"Picture4"],[UIImage imageNamed:@"Picture5"], nil];
 
+    Picture *picture1 = [[Picture alloc] initWithPicture:[UIImage imageNamed:@"Picture1"] andColor:[UIColor greenColor]];
+    Picture *picture2 = [[Picture alloc] initWithPicture:[UIImage imageNamed:@"Picture2"] andColor:[UIColor greenColor]];
+    Picture *picture3 = [[Picture alloc] initWithPicture:[UIImage imageNamed:@"Picture3"] andColor:[UIColor greenColor]];
+    Picture *picture4 = [[Picture alloc] initWithPicture:[UIImage imageNamed:@"Picture4"] andColor:[UIColor greenColor]];
+    Picture *picture5 = [[Picture alloc] initWithPicture:[UIImage imageNamed:@"Picture5"] andColor:[UIColor greenColor]];
+    
+    self.galleryPhotos = [NSMutableArray arrayWithObjects: picture1, picture2, picture3, picture4, picture5,  nil];
+    
 }
 
 -(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
-    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"PictureCell" forIndexPath:indexPath];
-
+    PictureCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"PictureCell" forIndexPath:indexPath];
+    
+    Picture *pictureForCell = self.galleryPhotos[indexPath.row];
+    cell.photoForCell.image = pictureForCell.picture;
     return cell;
 }
 
